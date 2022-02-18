37a44f98a0ceb9e149de36810ab75c969824bfec
NgocAnhDT <dtngocanh158@gmail.com>
2022-02-18 11:30:27 +0700

    create model & migration

---
+++ quicktask/app/Models/Teacher.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    use HasFactory;
+    protected $table = 'teachers';
+    protected $primaryKey = 'teacher_id';
+    protected $guarded = [];
+    
+    public function speciality() 
+    {
+        return $this->belongsTo(Speciality::class, 'id');
+    }
+}
---
+++ quicktask/database/migrations/2022_02_18_020130_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('teacher_id');
+            $table->string('fullname');
+            $table->string('avatar');
+            $table->integer('speciality_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('speciality_id')->references('id')->on('specialities')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
